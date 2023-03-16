99aff4df4913b7c0945fb9cc281398c778f37108
Tiago Nazari <tiagonazari5@gmail.com>
2023-03-16 14:27:19 -0300

    commit

--- main.py
+++ main.py
@@ -1,11 +1,123 @@
-from jogar_uma_rodada import comecar_nova_rodada
+from selecionar_palavra import carrega_palavra_secreta
 
-comecar_nova_rodada()
-fim = input("Deseja jogar novamente? (y/n):").split()[0]
-while fim not in "YyNn":
-    print("digite apenas 'y' ou 'n'.")
-    fim = input("Deseja jogar novamente? (y/n):").split()[0]
-if fim == ("y"):
-    comecar_nova_rodada()
-else:
-    exit()
+class JogoDeForca:
+    
+    def __init__(self):
+        self.palavra = carrega_palavra_secreta().upper()
+        self.tentativas = set()
+        self.chances = 7
+        self.tentativas.add(" ")
+        
+    def adivinhar(self, letra):
+        letra = letra.upper()
+        
+        if letra in self.tentativas:
+            print(f"Você já tentou a letra '{letra}', tente outra letra.")
+            return
+        
+        self.tentativas.add(letra)
+        
+        if letra not in self.palavra:
+            self.chances -= 1
+            print(f"A letra '{letra}' não está na palavra.")
+            print(f"Você tem {self.chances} chances restantes.")
+            return
+        
+        print(f"A letra '{letra}' está na palavra.")
+        
+        if self.ganhou():
+            print("Parabéns, você ganhou!")
+            return
+
+    def desenha_forca(self):
+        print("  _______     ")
+        print(" |/      |    ")
+
+        if(self.chances == 6):
+            print (" |      (_)   ")
+            print (" |            ")
+            print (" |            ")
+            print (" |            ")
+
+        if(self.chances == 5):
+            print (" |      (_)   ")
+            print (" |      \     ")
+            print (" |            ")
+            print (" |            ")
+
+        if(self.chances == 4):
+            print (" |      (_)   ")
+            print (" |      \|    ")
+            print (" |            ")
+            print (" |            ")
+
+        if(self.chances == 3):
+            print (" |      (_)   ")
+            print (" |      \|/   ")
+            print (" |            ")
+            print (" |            ")
+
+        if(self.chances == 2):
+            print (" |      (_)   ")
+            print (" |      \|/   ")
+            print (" |       |    ")
+            print (" |            ")
+
+        if(self.chances == 1):
+            print (" |      (_)   ")
+            print (" |      \|/   ")
+            print (" |       |    ")
+            print (" |      /     ")
+
+        if (self.chances == 0):
+            print (" |      (_)   ")
+            print (" |      \|/   ")
+            print (" |       |    ")
+            print (" |      / \   ")
+
+        print(" |            ")
+        print("_|___         ")
+        print()
+
+
+    def ganhou(self):
+        return set(self.palavra) <= self.tentativas
+    
+    def jogar(self):
+        self.print_linha()
+        print("Começando o jogo de forca!")
+        self.print_linha()
+        while self.chances > 0:
+            print()
+            print(f"Palavra: {self.get_palavra_mascarada()}")
+            letra = input("Digite uma letra: ")
+            self.adivinhar(letra)
+            self.desenha_forca()
+            self.print_linha()
+            if self.ganhou() or self.chances == 0:
+                print(f"A palavra era '{self.palavra}'.")
+                break
+        if self.ganhou():
+            self.jogar_novamente()
+        else:
+            print("Você perdeu!")
+            self.jogar_novamente()        
+
+        
+    def get_palavra_mascarada(self):
+        return "".join([letra if letra in self.tentativas else "-" for letra in self.palavra])
+    
+    def jogar_novamente(self):
+        resposta = input("Deseja jogar novamente? (s/n) ")
+        if resposta.lower() == "s":
+            self.__init__()
+            self.jogar()
+        else:
+            print("Obrigado por jogar!")
+            return
+    
+    def print_linha(self):
+        print("*****************************************************************************")
+
+jogo = JogoDeForca()
+jogo.jogar()
--- selecionar_palavra.py
+++ selecionar_palavra.py
@@ -1,37 +1,50 @@
 import random
+
 def carrega_palavra_secreta():
-    arquivo = open("tudo.txt", "r")
+    arquivo_das_palavras = ""
+    escolha_a_categoria = input("Escolha a categoria[futebol/frutas/paises]:").strip().lower()
+    if escolha_a_categoria == "futebol":
+        escolha_de_dificuldade = "Escolha um campeonato [serie a/serie b/serie c/champions]:"
+    elif escolha_a_categoria == "frutas":
+        escolha_de_dificuldade = "Escolha a dificuldade [facil/medio/dificil]:"
+    else:
+        escolha_de_dificuldade = "Escolha um continente [africa/americas/europa]:"
+
+    dificuldade_das_palavras = input(escolha_de_dificuldade).strip().lower()
+    
+    if dificuldade_das_palavras == "facil":
+        arquivo_das_palavras = "./palavras/frutas-facil.txt"
+    elif dificuldade_das_palavras == "medio":
+        arquivo_das_palavras = "./palavras/frutas-medio.txt"
+    elif dificuldade_das_palavras == "dificil":
+        arquivo_das_palavras = "./palavras/frutas-dificil.txt"
+    elif dificuldade_das_palavras == "serie a":
+        arquivo_das_palavras = "./palavras/times-seriea.txt"
+    elif dificuldade_das_palavras == "serie b":
+        arquivo_das_palavras = "./palavras/times-serieb.txt"
+    elif dificuldade_das_palavras == "serie c":
+        arquivo_das_palavras = "./palavras/times-seriec.txt"
+    elif dificuldade_das_palavras == "champions":
+        arquivo_das_palavras = "./palavras/times-champions.txt"
+    elif dificuldade_das_palavras == "africa":
+        arquivo_das_palavras = "./palavras/paises-africa.txt"
+    elif dificuldade_das_palavras == "americas":
+        arquivo_das_palavras = "./palavras/paises-americas.txt"
+    elif dificuldade_das_palavras == "europa":
+        arquivo_das_palavras = "./palavras/paises-europa.txt"
+
+    arquivo = open(arquivo_das_palavras, "r")
     palavras = []
 
     for linha in arquivo:
         linha = linha.strip()
         palavras.append(linha)
-
+    
     arquivo.close()
 
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
     return palavra_secreta
 
-
-def dica(palavra_secreta):
-    dica = " "
-    lista = ["frutas-dificil", "frutas-facil", "frutas-medio", "paises-africa", "paises-americas",
-             "paises-europa",
-             "times-champions", "times-seriea", "times-serieb", "times-seriec"]
-    for c in lista:
-        arquivo = open(c + ".txt", "r")
-        palavras = []
-
-        for linha in arquivo:
-            linha = linha.strip()
-            palavras.append(linha.upper())
-
-        arquivo.close()
-        if palavra_secreta in palavras:
-            dica = c
-    return dica
-
 #palavra_secreta = carrega_palavra_secreta()
-#dica = dica(palavra_secreta)
-#print(palavra_secreta,dica)
+#print(palavra_secreta)
